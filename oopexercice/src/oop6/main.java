4f7045bc1ade4bac9944c5185107f3d3506ea5fd
yassine71-star <moumenyassin998@gmail.com>
2020-12-09 01:13:57 +0100

    atelier done

--- oopexercice/src/oop6/main.java
+++ oopexercice/src/oop6/main.java
@@ -4,7 +4,16 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
+		        Account6 khalid = new Account6();
+		        khalid.setNom("khalid KARIMI");
+		        khalid.setAdresse("Casablanca");
+		        khalid.setSolde(7654);
+		        //Output
+		        System.out.println("Nom: " + khalid.getNom());
+		        System.out.println("Adresse: " + khalid.getAdresse());
+		        System.out.println("Solde: " + khalid.getSolde() + "DH");
+		        khalid.retirer();
+		    }
 	}
 
-}
